99659fccbf4e3d775873462621bd48a625a33a7c
talhamoveon9@gmail.com <talhamoveon9@gmail.com>
2025-05-08 23:53:03 +0500

    Remove multiple unnecessary files

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-
-    images: {
-        domains: ['images.unsplash.com'], // Allow images from Unsplash
-      },
-};
-
-export default nextConfig;
